4a414b65faa244fa8c1939d103278bd1bd14c38a
Kangina <85179015+kangina-tech@users.noreply.github.com>
2024-04-17 15:25:19 +0300

    finished calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  const arr = [];
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return arr;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+ return arr.reduce((prod, num) => prod * num);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if(num === 0) {
+    return 1;
+  }
+  let answer = 1;
+  for(let i = num; i > 0; i--) {
+    answer *= i;
+  }
+  return answer;
 };
 
 // Do not edit below this line
